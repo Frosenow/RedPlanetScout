4dc4a6d6ea36bc0fae314d4ab73f1cb97a48f808
Frosenow <filip.potepa@gmail.com>
2023-03-21 23:01:13 +0100

    responsive design added using grid

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default function Card({photo}){
     return (
-        <div className="card" id={photo.id}>
-            <img className="card-image" src={photo.img_src}></img>
-            <div className="card-description">
-                <ul>
-                    <li>Rover Name: {`${photo.rover.name}`}</li>
-                    <li>Earth Date: {`${photo.earth_date}`}</li>
-                    <li>Camera Name: {`${photo.camera.name} - ${photo.camera.full_name}`}</li>
-                    <li>Sol: {`${photo.sol}`}</li>
-                </ul>
+            <div className="card" id={photo.id}>
+                <img className="card-image" src={photo.img_src}></img>
+                <div className="card-description">
+                    <ul>
+                        <li className="card-title">Rover Name: {`${photo.rover.name}`}</li>
+                        <li>Earth Date: {`${photo.earth_date}`}</li>
+                        <li>Camera Name: {`${photo.camera.name} - ${photo.camera.full_name}`}</li>
+                        <li>Sol: {`${photo.sol}`}</li>
+                    </ul>
+                </div>
             </div>
-        </div>
     )
 }
